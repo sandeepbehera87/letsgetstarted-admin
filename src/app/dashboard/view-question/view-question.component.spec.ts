19cd190f35c12dd1374b8b474028e38b716608e5
Sandeep Behera <sandeep.jitu87@gmail.com>
2019-12-23 18:07:42 +0000

    Unit test

--- src/app/dashboard/view-question/view-question.component.spec.ts
+++ src/app/dashboard/view-question/view-question.component.spec.ts
@@ -3,7 +3,7 @@ import {ModalModule} from 'ngx-bootstrap';
 import {ModalComponent} from '../../../components/modal/modal.component';
 import {ViewQuestionComponent} from './view-question.component';
 
-xdescribe('ViewQuestionComponent', () => {
+describe('ViewQuestionComponent', () => {
   let component: ViewQuestionComponent;
   let fixture: ComponentFixture<ViewQuestionComponent>;
 
