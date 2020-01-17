59bd6b8a4f01aa221a89a871db005f901110af34
Sandeep Behera <sandeep.jitu87@gmail.com>
2020-01-17 15:12:39 +0000

    Adding withCredentials parameter

--- src/core/http-interceptors/questions-api-interceptor.ts
+++ src/core/http-interceptors/questions-api-interceptor.ts
@@ -22,6 +22,7 @@ export class QuestionsApiInterceptor implements HttpInterceptor {
       url: AppSettings.API_ENDPOINT + req.url,
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Credentials': 'true',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods':
           'GET, POST, PATCH, PUT, DELETE, OPTIONS',
@@ -30,6 +31,7 @@ export class QuestionsApiInterceptor implements HttpInterceptor {
           ? this.sharedService.apiToken
           : '',
       }),
+      withCredentials: true,
     });
     return next.handle(appReq);
   }
