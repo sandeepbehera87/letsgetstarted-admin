f69872fead94ac84a72a9b6a3ba30e55c2ecc16a
Sandeep Behera <sandeep.jitu87@gmail.com>
2020-01-22 14:43:03 +0000

    Preload stratgy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,25 +6,30 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () =>
       import('./register/register.module').then(m => m.RegisterModule),
+    data: {preload: true}, // preload flag
   },
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then(m => m.LoginModule),
+    data: {preload: true}, // preload flag
   },
   {
     path: 'dashboard',
     loadChildren: () =>
       import('./dashboard/dashboard.module').then(m => m.DashboardModule),
+    data: {preload: true}, // preload flag
   },
   {
     path: 'not-found',
     loadChildren: () =>
       import('../core/shared/shared.module').then(m => m.SharedModule),
+    data: {preload: true}, // preload flag
   },
   {
     path: 'session-expire',
     loadChildren: () =>
       import('../core/shared/shared.module').then(m => m.SharedModule),
+    data: {preload: true}, // preload flag
   },
   {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: '**', redirectTo: 'not-found', pathMatch: 'full'},
