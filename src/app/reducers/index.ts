80aa83fc8923769fe7b809056bd6b8315d78697e
Sandeep Behera <sandeep.jitu87@gmail.com>
2020-01-26 11:54:23 +0000

    Removing unwanted headers

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -5,12 +5,9 @@ import {
   createSelector,
   MetaReducer,
 } from '@ngrx/store';
-import {environment} from 'src/environments/environment';
 
 export interface AppState {}
 
 export const reducers: ActionReducerMap<AppState> = {};
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production
-  ? []
-  : [];
+export const metaReducers: MetaReducer<AppState>[] = [];
