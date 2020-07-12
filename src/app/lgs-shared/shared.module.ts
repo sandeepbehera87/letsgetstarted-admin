1a17b49b3df448771ab6cad322d4b7baa8599913
Sandeep Behera <sandeep.jitu87@gmail.com>
2020-07-12 19:25:35 +0100

    NG 10 upgrade

--- src/app/lgs-shared/shared.module.ts
+++ src/app/lgs-shared/shared.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // TODO : Remove FormsModule
 import { ReactiveFormsModule } from '@angular/forms';
